5929a422463853fbd8f7589c322a391be674f740
Ekaterina Chavan <144109471+t0kyolover@users.noreply.github.com>
2024-03-21 12:59:01 +0000

    now it is

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -9,8 +9,8 @@ export const Navbar = () => {
           <img
             src="https://upload.wikimedia.org/wikipedia/en/thumb/3/37/Just_Eat_Takeaway.com_icon_logo.svg/1200px-Just_Eat_Takeaway.com_icon_logo.svg.png"
             alt="Just Eat"
-            width="30"
-            height="24"
+            width="44"
+            height="40"
           />
         </Link>
       </div>
